51912d8625c8504ffef8a1707502cc840d0a0d6d
After-HeavyRain <dayu-gh@qq.com>
2026-08-16 15:33:37 +0800

    Fix maven 401.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -29,7 +29,7 @@ dependencyResolutionManagement {
             name = "GitHubPackages (Bitwarden)"
             url = uri("https://maven.pkg.github.com/bitwarden/sdk")
             credentials {
-                username = ""
+                username = userProperties["gitHubUser"] as String? ?: System.getenv("GITHUB_ACTOR")
                 password = userProperties["gitHubToken"] as String? ?: System.getenv("GITHUB_TOKEN")
             }
         }
